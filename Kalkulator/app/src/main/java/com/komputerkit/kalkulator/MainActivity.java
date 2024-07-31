a5bfaaf7fafc6cb31fe274885268c70262f032ab
Athaa18 <athallahdf@gmail.com>
2024-07-31 23:37:30 +0700

    Video 8
    Android

--- Kalkulator/app/src/main/java/com/komputerkit/kalkulator/MainActivity.java
+++ Kalkulator/app/src/main/java/com/komputerkit/kalkulator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.komputerkit.kalkulator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvhasil;
+    EditText etbil_1, etBil_2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        load();
+    }
+
+    public void load(){
+        tvhasil = findViewById(R.id.tvHasil);
+        etbil_1 = findViewById(R.id.etBil_1);
+        etBil_2 = findViewById(R.id.etBil_2);
+    }
+
+    public void btnJumlah(View view) {
+
+        if (etbil_1.getText().toString().equals("")|| etBil_2.getText().toString().equals("")) {
+            Toast.makeText(this, "Ada Bilangan Yang Kosong", Toast.LENGTH_SHORT).show();
+        } else{
+
+            double bil_1 = Double.parseDouble(etbil_1.getText().toString());
+        double bil_2 = Double.parseDouble(etBil_2.getText().toString());
+
+        double hasil = bil_1 + bil_2;
+
+        tvhasil.setText(hasil + "");
+        }
     }
 }
